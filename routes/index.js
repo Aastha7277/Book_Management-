2d400ca3090a51579217639a39416b5cc9402bbd
Aastha <aastha.kumari.7277@gmail.com>
2025-04-10 00:09:22 +0530

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/user', require('./auth'));
-router.use('/book', require('./book'));
+router.use('/book', require('./books'));
 
 module.exports = router;
